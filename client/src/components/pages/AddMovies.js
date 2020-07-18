319ebb4666a3cfc2f07a78f04de5337bc03b5fd6
Jee Zhou <jwhiz113@gmail.com>
2020-07-18 00:20:45 -0500

    working on addmovies

--- client/src/components/pages/AddMovies.js
+++ client/src/components/pages/AddMovies.js
@@ -7,22 +7,25 @@ import Axios from 'axios';
 export const AddMovies = () => {
     
     const history = useHistory();
-    const [searchMovie, setSearchMovie] = useState('');
+    const [movieName, setMovieName] = useState('');
     const [movieData, setMovie] = useState({});
     //const [password, setPassword] = useState("");*/
-    
+    //console.log(movieName);
     const searchMovies = query =>{
         API.search(query)
-        .then((res)=>{
-            console.log(res.data);
+        .then((res)=>{  //Why the heck does it lag behind one???
+            setMovie({movieData:res.data});
+            console.log(movieData);
+            //console.log(movieName);
         });
     }
-
+    //Idea now: searchMovie will be updated and then moviedata is called when the search button is hit.  
+    //Now there should not need to be a useeffect function in here.
     const submit = e =>{
         e.preventDefault();
-        searchMovies("Shrek");
+        searchMovies(movieName);
     }
-
+    //Once the problem above is fixed, work on a botton to add to the user's data base.
   return (
     <div>
       <div className='field'>
@@ -31,7 +34,7 @@ export const AddMovies = () => {
             className='input'
             type='text'
             placeholder='Movie Name'
-            onChange={(e) => setSearchMovie(e.target.value)}
+            onChange={(e) => setMovieName(e.target.value)}
           />
           <span className='icon is-small is-left'>
             <i className='fas fa-envelope' />
