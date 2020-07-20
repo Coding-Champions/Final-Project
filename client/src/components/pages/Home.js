e0d073ccc850824b278aa0b54df16cbd55979ac0
Ray H <rjhern37@neiu.edu>
2020-07-19 21:55:41 -0500

    Added card, AOS to home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,14 +4,25 @@ import {Link} from "react-router-dom"
 const Home = () =>{
     return (
         <>
-            <div className="home-container">
-                <Link to="/login">
-                    <button className="button">Login</button>
-                </Link>
-                <Link to="/register">
-                    <button className="button">Register</button>
-                </Link>
-            </div>
+            <div data-aos="zoom-in-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000">
+                 <div className="login-container">
+                    <div className="card">
+                        <header className="card-header">
+                        <p className="card-header-title">
+                        <h3><strong>Welcome to Scene It</strong></h3>
+                        </p>
+                        </header>
+                        <div className="home-container">
+                            <Link to="/login">
+                                <button className="button">Login</button>
+                            </Link>
+                            <Link to="/register">
+                                <button className="button">Register</button>
+                            </Link>
+                        </div>
+                     </div>
+                 </div>
+             </div>
         </>
     )
  }
