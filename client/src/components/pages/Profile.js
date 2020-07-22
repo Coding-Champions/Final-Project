c68c30fe90a28a7b9d232937c9da73a15dae79b8
rodriguezh21 <rodriguezh21@gmail.com>
2020-07-22 11:08:19 -0500

    fixing friends buttons

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -71,7 +71,7 @@ export const Profile = () => {
             friendsList.map(friend => (
               <a href={'friend/' + friend.id}>
                 {' '}
-                <button className='button friends-button' style={({ color: 'white' }, { textTransform: 'capitalize' })}>{friend.name}</button>
+                <button className='button friends-button' style={( { textTransform: 'capitalize' })}>{friend.name}</button>
               </a>
             ))
           ) : (
